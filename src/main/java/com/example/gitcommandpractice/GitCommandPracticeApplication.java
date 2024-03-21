cedc78ae2bcba30807d05727afe2a0ebe46c56ba
yoowonjun <twojunspace@naver.com>
2024-03-22 02:43:36 +0900

    Initial commit

---
+++ src/main/java/com/example/gitcommandpractice/GitCommandPracticeApplication.java
@@ -0,0 +1,12 @@
+package com.example.gitcommandpractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitCommandPracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitCommandPracticeApplication.class, args);
+	}
+}
